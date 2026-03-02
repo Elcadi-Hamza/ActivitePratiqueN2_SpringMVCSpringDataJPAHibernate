083b332822570f9b884e77fe4b0778d737c69edb
elcadiHamza <email_du_compte_rick679>
2026-03-02 12:30:37 +0000

    init : creating the project

---
+++ src/main/java/org/example/activiterpratique02/ActiviterPratique02Application.java
@@ -0,0 +1,13 @@
+package org.example.activiterpratique02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActiviterPratique02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActiviterPratique02Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/activiterpratique02/ActiviterPratique02ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.activiterpratique02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActiviterPratique02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
